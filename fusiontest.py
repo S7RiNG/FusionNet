d450b42c8375fe6c7d6a642e8ee9773c37d91147
harrier <925749859@qq.com>
2024-12-15 14:07:15 +0800

    add something

--- fusiontest.py
+++ fusiontest.py
@@ -17,9 +17,17 @@ if __name__ == "__main__":
         data = r'/Users/harrier/Work/kitti/yolo_fusion/data.yaml'
         device = 'mps'
     
-    model = FusionNet('ultralytics/cfg/models/v8/yolov8m-fusion.yaml', verbose=True)
+    # model = FusionNet('ultralytics/cfg/models/v8/yolov8m-fusion.yaml', verbose=True)
+    # torchinfo.summary(model)
+    # res = model.train(data=data, device=device, epochs=100, batch=2, cache='disk', workers= 4)
+
+    # model = FusionNet(r'E:\Work\stu\FusionNet\runs\detect\train\weights\last.pt')
+    # torchinfo.summary(model)
+    # res = model.train(resume=True)
+
+    model = FusionNet(r'E:\Work\stu\FusionNet\runs\detect\train\weights\best.pt')
     torchinfo.summary(model)
-    res = model.train(data=data, device=device, epochs=100, batch=2, cache='disk', workers= 4)
+    res = model.val(data=data, device=device, batch=2, cache='disk')
 
     # model = YOLO('ultralytics/cfg/models/v8/yolov8m.yaml', verbose=True)
     # torchinfo.summary(model)
--- ultralytics/models/fusion/model.py
+++ ultralytics/models/fusion/model.py
@@ -19,3 +19,38 @@ class FusionNet(Model):
                 "model": FusionNetModel,
             }
         }
+    
+    def val(
+        self,
+        validator=None,
+        **kwargs,
+    ):
+        """
+        Validates the model using a specified dataset and validation configuration.
+
+        This method facilitates the model validation process, allowing for customization through various settings. It
+        supports validation with a custom validator or the default validation approach. The method combines default
+        configurations, method-specific defaults, and user-provided arguments to configure the validation process.
+
+        Args:
+            validator (ultralytics.engine.validator.BaseValidator | None): An instance of a custom validator class for
+                validating the model.
+            **kwargs (Any): Arbitrary keyword arguments for customizing the validation process.
+
+        Returns:
+            (ultralytics.utils.metrics.DetMetrics): Validation metrics obtained from the validation process.
+
+        Raises:
+            AssertionError: If the model is not a PyTorch model.
+
+        Examples:
+            >>> model = YOLO("yolo11n.pt")
+            >>> results = model.val(data="coco8.yaml", imgsz=640)
+            >>> print(results.box.map)  # Print mAP50-95
+        """
+        args = {**self.overrides, **kwargs, "mode": "val"}  # highest priority args on the right
+
+        validator = (validator or self._smart_load("validator"))(args=args, _callbacks=self.callbacks)
+        validator(model=self.model)
+        self.metrics = validator.metrics
+        return validator.metrics
